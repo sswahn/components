5a387881fa6913c9f4ec5fd702dc0e22c66ece7f
sswahn <36422765+sswahn@users.noreply.github.com>
2023-11-01 17:59:46 -0400

    Create Checkbox.js

---
+++ src/core/Checkbox/Checkbox.js
@@ -0,0 +1,25 @@
+import { useRef } from 'react'
+
+const Checkbox = ({ label, checked, onChange }) => {
+  const checkboxRef = useRef(null)
+
+  const handleClick = event => {
+    checkboxRef.current.click()
+  }
+
+  // move svgs to their own components
+  
+  return (
+    <div className="checkbox-container">
+      <label className="checkbox-label" htmlFor="checkbox">{label}</label>
+      <input id="checkbox" className="checkbox" ref={checkboxRef} type="checkbox" checked={checked} onChange={onChange} aria-checked={checked} />
+      checked 
+        ? <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zM337 209L209 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L303 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"/></svg> 
+        : <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M384 80c8.8 0 16 7.2 16 16V416c0 8.8-7.2 16-16 16H64c-8.8 0-16-7.2-16-16V96c0-8.8 7.2-16 16-16H384zM64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64z"/></svg>
+       } 
+      onClick={handleClick} />
+    </div>
+  )
+}
+
+export default Checkbox
