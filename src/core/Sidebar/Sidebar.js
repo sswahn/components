7b1223ebf83dc96f99bb16375437ba029fce58fb
sswahn <36422765+sswahn@users.noreply.github.com>
2023-11-01 16:48:45 -0400

    Update Sidebar.js

--- src/core/Sidebar/Sidebar.js
+++ src/core/Sidebar/Sidebar.js
@@ -1,1 +1,23 @@
-// --
+import { useContext, lazy } from 'react'
+import { Context } from '../../Provider'
+import styles from 'styles.module.css'
+
+const SideBar = ({ children }) => {
+  const [context, dispatch] = useContext(Context)
+  
+  const handleSidebar = event => {
+    dispatch({ type: 'sidebar', payload: false })
+  }
+  
+  return (
+    <>
+      <nav id="sidebar" className={`${styles.sidebar} ${context.sidebar ? 'sidebar-open : 'sidebar-closed'}`}>
+        {/* make off click close functionality */}
+        {children}
+      </nav>
+      {/* isSidebarOpen && <div className="overlay" onClick={toggleSidebar} /> */} {/* Overlay for closing sidebar */}
+    </>
+  )
+}
+
+export default Sidebar
