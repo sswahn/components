289d9e42aa3534fef56887a4bc3ee72d0a9c6f39
sswahn <36422765+sswahn@users.noreply.github.com>
2023-11-02 06:50:40 -0400

    Update Dropdown.js

--- src/core/Dropdown/Dropdown.js
+++ src/core/Dropdown/Dropdown.js
@@ -1,1 +1,28 @@
-//--
+import { useState } from 'react'
+
+const Dropdown = ({ text, options }) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen)
+  }
+
+  return (
+    <div className="dropdown">
+      <button className="dropbtn" onClick={toggleDropdown}>
+        {text}
+      </button>
+      {isOpen && (
+        <div className="dropdown-content">
+          {options.map((option, index) => (
+            <button type="button" key={index} onClick={option.onClick}>
+              {option.label}
+            </button>
+          ))}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Dropdown
