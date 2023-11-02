819ea50260872afb896bc61622073d58a115177e
sswahn <36422765+sswahn@users.noreply.github.com>
2023-11-02 14:43:45 -0400

    Update Avatar.js

--- src/core/Avatar/Avatar.js
+++ src/core/Avatar/Avatar.js
@@ -1,35 +1,23 @@
 import { useState } from 'react'
-
 import UserIcon from './UserIcon'
 
-export default function Avatar({ src, username }) {
-  const [context, dispatch] = useContext(Context)
-
-
-  // need to handle close modals
-  const handleNavigation = event => {
-    event.preventDefault()
-    const route = `/profile/${username.toLowerCase()}`
-    if (window.location.pathname !== route) {
-      history.pushState(null, '', route)
-      dispatch({ type: 'router', payload: route })
-    }
-  }
-
+const Avatar = ({ image, username, onClick, badge }) => {
   return (
-    <a className="avatar" href={`/profile/${username.toLowerCase()}`} onClick={handleNavigation} aria-label={`${username}'s' profile`}>
-      {src 
-        ? <img src={src} alt={`${username}'s' avatar`} />
-        : <FontAwesomeIcon icon={faCircleUser} aria-label="default avatar image" />
+    <a className="avatar" href={`/profile/${username.toLowerCase()}`} onClick={onClick} aria-label={`${username}'s' profile`}>
+      {image 
+        ? <img src={image} alt={`${username}'s' avatar`} />
+        : <UserIcon />
       }
-      {/* context.user.isPro && ( */}
+      {badge &&
         <div className="pro-badge" aria-label="pro badge">
           <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 100 100">
             <circle cx="50" cy="50" r="44" fill="#00bfff" stroke="white" strokeWidth="9" />
             <path d="M35 50L45 60L65 35" stroke="#fff" strokeWidth="8" fill="none" />
           </svg>
         </div>
-      
+      }
     </a>
   )
 }
+
+export default Avatar
