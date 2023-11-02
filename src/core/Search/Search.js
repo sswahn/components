1dd0e8233ad06d7ad16157df65b2e1aef76412b0
sswahn <36422765+sswahn@users.noreply.github.com>
2023-11-02 09:15:59 -0400

    Update Search.js

--- src/core/Search/Search.js
+++ src/core/Search/Search.js
@@ -1,10 +1,25 @@
 import { useState } from 'react'
 import styles from './styles.module.css'
 
-const Search = ({ className }) => {
+const Search = ({ className, onChange, placeholder }) => {
+  const [searchValue, setSearchValue] = useState('')
 
+  const handleOnChange = (event) => {
+    const newValue = event.target.value
+    setSearchValue(newValue)
+    if (onChange) {
+      onChange(newValue)
+    }
+  }
+  
   return (
-    <input className={`${styles.search} ${className}`} type="text" //>
+    <input 
+      className={`${styles.search} ${className}`} 
+      type="text"
+      value={searchValue}
+      onChange={handleOnChange}
+      placeholder={placeholder || ''}
+    />
   )
 }
 
