b2696be76e5645e23cd9a030434d99600c25febe
sswahn <36422765+sswahn@users.noreply.github.com>
2023-11-02 15:00:15 -0400

    Update Avatar.js

--- src/core/Avatar/Avatar.js
+++ src/core/Avatar/Avatar.js
@@ -1,21 +1,16 @@
 import { useState } from 'react'
 import UserIcon from './UserIcon'
+import BadgeIcon from './BadgeIcon'
+import styles from './styles.module.css'
 
 const Avatar = ({ image, username, onClick, badge }) => {
   return (
-    <a className="avatar" href={`/profile/${username.toLowerCase()}`} onClick={onClick} aria-label={`${username}'s' profile`}>
+    <a className={styles.avatar} href={`/profile/${username.toLowerCase()}`} onClick={onClick} aria-label={`${username}'s' profile`}>
       {image 
         ? <img src={image} alt={`${username}'s' avatar`} />
         : <UserIcon />
       }
-      {badge &&
-        <div className="pro-badge" aria-label="pro badge">
-          <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 100 100">
-            <circle cx="50" cy="50" r="44" fill="#00bfff" stroke="white" strokeWidth="9" />
-            <path d="M35 50L45 60L65 35" stroke="#fff" strokeWidth="8" fill="none" />
-          </svg>
-        </div>
-      }
+      {badge &&  }
     </a>
   )
 }
