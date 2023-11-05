2a70f7b5a0e972e78d4aa0f25677bc73b4a599d6
sswahn <36422765+sswahn@users.noreply.github.com>
2023-11-05 14:14:51 -0500

    Update Sidebar.js

--- src/core/Sidebar/Sidebar.js
+++ src/core/Sidebar/Sidebar.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { Context } from '../../Provider'
+import { Context } from '../../Provider.js'
 import styles from 'styles.module.css'
 
 const SideBar = ({ children, className }) => {
