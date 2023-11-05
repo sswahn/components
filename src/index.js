a1462a02092538c0d64469f4d8c6124f112fcac0
sswahn <36422765+sswahn@users.noreply.github.com>
2023-11-05 15:38:40 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import Avatar from './Avatar/Avatar.js'
-import Checkbox from './Checkbox/Checkbox.js'
-import Dropdown from './Dropdown/Dropdown.js'
-import Modal from './Modal/Modal.js'
-import Search from './Search/Search.js'
-import Sidebar from './Sidebar/Sidebar.js'
+import Avatar from './core/Avatar/Avatar.js'
+import Checkbox from './core/Checkbox/Checkbox.js'
+import Dropdown from './core/Dropdown/Dropdown.js'
+import Modal from './core/Modal/Modal.js'
+import Search from './core/Search/Search.js'
+import Sidebar from './core/Sidebar/Sidebar.js'
 
 export {
   Avatar,
