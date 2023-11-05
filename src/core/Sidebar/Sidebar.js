ba48556a3d942cc7be39fe5e709b31507301053e
sswahn <36422765+sswahn@users.noreply.github.com>
2023-11-05 15:47:34 -0500

    Update Sidebar.js

--- src/core/Sidebar/Sidebar.js
+++ src/core/Sidebar/Sidebar.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { Context } from '../../Provider.js'
-import styles from 'styles.module.css'
+import styles from './styles.module.css'
 
 const SideBar = ({ children, className }) => {
  const [isOpen, setIsOpen] = useState(false)
