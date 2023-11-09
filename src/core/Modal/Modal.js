12a0abdebb3e5d84720c41d31ea07bb5b3084df3
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-09 13:46:25 -0500

    Update Modal.js

--- src/core/Modal/Modal.js
+++ src/core/Modal/Modal.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import styles from './styles.module.css'
 
 const Modal = ({ className, open, onClose, children }) => {
