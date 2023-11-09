668729b5b14cc248205ac46609d5407cb51dad40
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-09 18:58:49 -0500

    Update Dropdown.js

--- src/core/Dropdown/Dropdown.js
+++ src/core/Dropdown/Dropdown.js
@@ -1,19 +1,33 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import styles from './styles.module.css'
 
 const Dropdown = ({ className, text, options }) => {
   const [isOpen, setIsOpen] = useState(false)
-
+  const menuRef = useRef(null)
+  
   const toggleDropdown = () => {
     setIsOpen(!isOpen)
   }
+
+    const clickToClose = event => {
+    if (event.target === menuRef.current) {
+      closeModal()
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('mousedown', clickToClose)
+    return () => {
+      document.removeEventListener('mousedown', clickToClose)
+    }
+  }, [])
 
   return (
     <div className={`${styles.dropdown} ${className}`}>
       <button type="button" onClick={toggleDropdown} aria-label="dropdown button" aria-haspopup="true" aria-expanded={isOpen}>
         {text}
       </button>
-      <menu style={{display: isOpen ? 'block' : 'none'}} aria-hidden={!isOpen}>
+      <menu style={{display: isOpen ? 'block' : 'none'}} ref={menuRef} aria-hidden={!isOpen}>
         {options.map((option, index) => (
           <li key={index} onClick={option.onClick} role="menuitem">
             {option.label}
