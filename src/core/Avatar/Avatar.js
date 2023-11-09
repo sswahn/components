e64f0c44c45afc136d4bdfcd5c0890efcb111364
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-09 11:13:26 -0500

    Update Avatar.js

--- src/core/Avatar/Avatar.js
+++ src/core/Avatar/Avatar.js
@@ -3,18 +3,13 @@ import UserIcon from './UserIcon.js'
 import BadgeIcon from './BadgeIcon.js'
 import styles from './styles.module.css'
 
-const Avatar = ({ className, image, username, onClick, badge }) => {
+const Avatar = ({ className, image, username, onClick }) => {
   return (
     <div className={`${styles.avatar} ${className}`} onClick={onClick} aria-label={`${username}'s avatar`}>
       {image 
         ? <img src={image} alt={`${username}'s avatar`} />
         : <UserIcon />
       }
-      {badge && (
-        <div className={styles.badge}>
-          <BadgeIcon />
-        </div>
-      )}
     </div>
   )
 }
