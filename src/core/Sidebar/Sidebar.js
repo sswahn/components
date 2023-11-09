0f9ee4e71393304ec4e92971c143127c08760afe
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-09 14:54:54 -0500

    Update Sidebar.js

--- src/core/Sidebar/Sidebar.js
+++ src/core/Sidebar/Sidebar.js
@@ -40,7 +40,7 @@ const Sidebar = ({ className, open, onClose, children }) => {
       <nav className={`${styles.sidebar} ${className} ${isOpen ? styles.open : styles.closed}`} ref={sidebarRef}>
         {children}
       </nav>
-      {isOpen && <div className={styles.overlay} onClick={closeSidebar} />}
+      {isOpen && <div className={`${styles.overlay} ${styles.open}`} onClick={closeSidebar} />}
     </>
   )
 }
