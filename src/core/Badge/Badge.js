f30b10e102fe8d0a233757325d4dfcd577fa1090
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-10 01:19:33 -0500

    Update Badge.js

--- src/core/Badge/Badge.js
+++ src/core/Badge/Badge.js
@@ -1,1 +1,11 @@
-// icon count badge
+import { useState } from 'react'
+import styles from './styles.module.css'
+
+const Badge = ({ children }) => {
+
+  return (
+    <div className="badge" role="status" aria-label="notification indicator" aria-hidden="false">
+      {children}
+    </div>
+  )
+}
