a6c1619f4c3db33998f2adc5ff080bb491af7833
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-10 14:08:16 -0500

    Update Sidebar.js

--- src/core/Sidebar/Sidebar.js
+++ src/core/Sidebar/Sidebar.js
@@ -38,7 +38,7 @@ const Sidebar = ({ className, open, onClose, children }) => {
   
   return (
     <>
-      <nav className={`${styles.sidebar} ${className} ${isOpen ? styles.open : styles.closed}`} ref={sidebarRef} aria-label="collapsible sidebar" aria-hidden={!isOpen}>
+      <nav className={`${styles.sidebar} ${className || ''} ${isOpen ? styles.open : styles.closed}`} ref={sidebarRef} aria-label="collapsible sidebar" aria-hidden={!isOpen}>
         {children}
       </nav>
       {/* isOpen && <div className={`${styles.overlay}`} onClick={closeSidebar} /> */}
