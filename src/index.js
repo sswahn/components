661ee1ae80b34cd8ab187d5196ef020c97a7a8d5
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-18 14:57:48 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import Dropdown from './core/Dropdown/Dropdown.js'
 import Modal from './core/Modal/Modal.js'
 import Search from './core/Search/Search.js'
 import Sidebar from './core/Sidebar/Sidebar.js'
+import Tabs from './core/Tabs/Tabs.js'
 
 export {
   Checkbox,
   Dropdown,
   Modal,
   Search,
-  Sidebar
+  Sidebar,
+  Tabs
 }
