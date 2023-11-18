2a42cb32931ffd0b63bd8b1249061a6b68855d93
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-18 14:50:22 -0500

    Create Tabs.js

---
+++ src/core/Tabs/Tabs.js
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import styles from './styles.module.css'
+
+const Tabs = ({ className, options }) => {
+  const [active, setActive] = useState(0)
+
+  const handleTabClick = tabNumber => {
+    setActive(tabNumber)
+  }
+
+  return (
+    <div>
+      <div className={`${styles.tabs} ${className || ''}`} role="tablist" aria-label="tab navigation">
+        {options.map((tab, index) => (
+          <button 
+            key={index} 
+            id={`tab-${index}`}
+            className={active === index ? 'active' : ''} 
+            onClick={() => handleTabClick(index)} 
+            type="button" 
+            role="tab" 
+            aria-selected={active === index} 
+            aria-controls={`tabpanel-${index}`}>
+            {tab.name}
+          </button>
+        ))}
+      </div>
+      <div 
+        id={`tabpanel-${active}`}
+        className={styles.content} 
+        role="tabpanel"
+        aria-labelledby={`tab-${active}`}>
+        {options[active].content}
+      </div>
+    </div>
+  )
+}
+
+export default Tabs
